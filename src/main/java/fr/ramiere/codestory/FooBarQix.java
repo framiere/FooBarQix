2b0f02c1cf9f350f37c4d0beec967c6ab2028be3
Florent Ramière <florent@ramiere.com>
2011-11-18 22:37:56 +0100

    first draft for CodeStory

---
+++ src/main/java/fr/ramiere/codestory/FooBarQix.java
@@ -0,0 +1,62 @@
+package fr.ramiere.codestory;
+
+public class FooBarQix {
+    private static final String QIX = "Qix";
+    private static final String BAR = "Bar";
+    private static final String FOO = "Foo";
+
+    public void displayUpTo(int number) {
+        if (number <= 0) {
+            throw new IllegalArgumentException("number should be strictly positive, got " + number);
+        }
+        for (int i = 1; i <= number; i++) {
+            System.out.println(convert(i));
+        }
+    }
+
+    public String convert(int number) {
+        final String value = Integer.toString(number);
+        final StringBuffer ret = new StringBuffer();
+
+        // divisible
+        boolean foo = number % 3 == 0;
+        boolean bar = number % 5 == 0;
+        boolean qix = number % 7 == 0;
+
+        // divisibles rule
+        ret.append(foo ? FOO : "");
+        ret.append(bar ? BAR : "");
+        ret.append(qix ? QIX : "");
+
+        // presence rule
+        foo |= value.contains("3");
+        bar |= value.contains("5");
+        qix |= value.contains("7");
+
+        // no conversion to be done ? get out
+        if (!foo && !bar && !qix) {
+            return value;
+        }
+
+        // iterate on the digits and apply corresponding conversions
+        final int length = value.length();
+        for (int i = 0; i < length; i++) {
+            final char digit = value.charAt(i);
+            switch (digit) {
+            case '3':
+                ret.append(foo ? FOO : digit);
+                break;
+            case '5':
+                ret.append(bar ? BAR : digit);
+                break;
+            case '7':
+                ret.append(qix ? QIX : digit);
+                break;
+            default:
+                // no op
+                break;
+            }
+        }
+        return ret.toString();
+    }
+}
---
+++ src/test/java/fr/ramiere/codestory/FooBarQixTest.java
@@ -0,0 +1,92 @@
+package fr.ramiere.codestory;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class FooBarQixTest {
+
+    FooBarQix fooBarQix;
+
+    @Before
+    public void setup() {
+        fooBarQix = new FooBarQix();
+    }
+
+    @Test
+    public void one_to_ten() {
+        assertThat(fooBarQix.convert(1)).isEqualTo("1");
+        assertThat(fooBarQix.convert(2)).isEqualTo("2");
+        assertThat(fooBarQix.convert(3)).isEqualTo("FooFoo");
+        assertThat(fooBarQix.convert(4)).isEqualTo("4");
+        assertThat(fooBarQix.convert(5)).isEqualTo("BarBar");
+        assertThat(fooBarQix.convert(6)).isEqualTo("Foo");
+        assertThat(fooBarQix.convert(7)).isEqualTo("QixQix");
+        assertThat(fooBarQix.convert(8)).isEqualTo("8");
+        assertThat(fooBarQix.convert(9)).isEqualTo("Foo");
+        assertThat(fooBarQix.convert(10)).isEqualTo("Bar");
+    }
+
+    @Test
+    public void dividers_first() {
+        assertThat(fooBarQix.convert(51)).isEqualTo("FooBar");
+    }
+
+    @Test
+    public void digit_orders() {
+        assertThat(fooBarQix.convert(53)).isEqualTo("BarFoo");
+    }
+
+    @Test
+    public void divider_orders() {
+        assertThat(fooBarQix.convert(21)).isEqualTo("FooQix");
+    }
+
+    @Test
+    public void contains_3() {
+        assertThat(fooBarQix.convert(13)).isEqualTo("Foo");
+    }
+
+    @Test
+    public void divisible_by_3_and_5_contains_5() {
+        assertThat(fooBarQix.convert(15)).isEqualTo("FooBarBar");
+    }
+
+    @Test
+    public void contains_double_3_and_is_divisible_by_3() {
+        assertThat(fooBarQix.convert(33)).isEqualTo("FooFooFoo");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void invalid_display() {
+        fooBarQix.displayUpTo(0);
+    }
+
+    @Test
+    public void display_one_to_one_thousand() {
+        final ByteArrayOutputStream output = new ByteArrayOutputStream();
+        final PrintStream out = System.out;
+        System.setOut(new PrintStream(output));
+        try {
+            fooBarQix.displayUpTo(10);
+            assertThat(output.toString()).isEqualTo("" //
+                    + "1\n"//
+                    + "2\n"//
+                    + "FooFoo\n"//
+                    + "4\n"//
+                    + "BarBar\n"//
+                    + "Foo\n"//
+                    + "QixQix\n"//
+                    + "8\n"//
+                    + "Foo\n"//
+                    + "Bar\n"//
+            );
+        } finally {
+            System.setOut(out);
+        }
+    }
+}
