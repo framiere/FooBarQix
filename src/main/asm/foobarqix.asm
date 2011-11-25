de264bc62633a3b07d2c2e0d156cb6b37b0912f8
Florent Ramière <florent@ramiere.com>
2011-11-26 00:12:01 +0100

    damn mac os alignment problems, refactoring with macros

--- src/main/asm/foobarqix.asm
+++ src/main/asm/foobarqix.asm
@@ -1,28 +1,51 @@
 ;
-; nasm -f macho foobarqix.asm
-; nasm -f macho methods.asm
-; ld -o foobarqix -e start methods.o foobarqix.o
+; nasm -g -f macho foobarqix.asm
+; nasm -g -f macho methods.asm
+; gcc -arch i386 -o foobarqix  methods.o foobarqix.o
 ;
 
 section .text
 
+; ------------------------------
+; OS X uses _ for symbol prefix
+extern _printf
+
+; ------------------------------
+; from methods.asm
+extern print_digit
 extern write
 extern exit
 
-global start                ; make the main function externally visible
+; ------------------------------
+%define stdout  1
 
-start:
+global _main				; make the main function externally visible
+
+; ------------------------------
+_main:
+    push    ebp
+    mov     ebp, esp
+
+	push dword 10      
+	push dword digit_format        
+  	call    _printf
+
 	; write
-    push dword foobarqix_length                           
-    push dword foobarqix_string
-    push dword 1              ; sysout
+	push dword foobarqix_length						   
+	push dword foobarqix_string
+	push dword stdout
 	call write
-    add esp, 12				  ; 3 args * 4 bytes/arg 
-    
-    ; exit 
-    push dword 0              
-    call exit 
+	add esp, 12			   	; 3 args * 4 bytes/arg 
+	
+	; exit 
+	push dword 4
+	mov eax, 4			  
+	call exit 
 
+
+; ------------------------------
 section .data
+
   foobarqix_string db "foo bar qix", 10, 0
   foobarqix_length equ $-foobarqix_string
+  digit_format db '%10d', 10, 0	
--- src/main/asm/methods.asm
+++ src/main/asm/methods.asm
@@ -1,16 +1,29 @@
 section .text
 
+; ------------------------------
+; OS X uses _ for symbol prefix
+extern _fprintf
+
+; ------------------------------
 global write
 global exit
 
+; ------------------------------
+%define system_call		0x80
+%define system_write	0x4
+%define system_exit  	0x1
+
+; ------------------------------
 section .text
 
 write:
-    mov eax, 0x4              ; system call write code
-    int 0x80                  ; make system call
+    mov eax, system_write              ; system call write code
+    int system_call                  ; make system call
     ret
 
 exit:
-    mov eax, 0x1              ; system call exit code
-    int 0x80                  ; make system call
-    ; no need to return
+    mov eax, system_exit              ; system call exit code
+    int system_call                  ; make system call
+    ret
+
+digit_format db '%10d', 0    
