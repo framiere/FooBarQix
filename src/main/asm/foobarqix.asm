5e1dc72879c00844253dd52255edc7e3417d8a2f
Florent Ramière <florent@ramiere.com>
2011-11-29 00:45:24 +0100

    having a hard time understanding stack alignments

--- src/main/asm/foobarqix.asm
+++ src/main/asm/foobarqix.asm
@@ -20,6 +20,7 @@ section .text
 extern _printf
 extern _sprintf
 extern _malloc
+extern _strcpy
 
 ; ------------------------------
 %define system_call		0x80
@@ -57,16 +58,16 @@ _main:
     push edi	
 
 ; buffer = malloc(42) 
-	sub 	esp, 8
+	sub 	esp, 1*4 + 4
  	push	dword 42
 	call	_malloc
-	add 	esp,8
+	add 	esp, 1*4 + 4
 	test	eax, eax
-	jz		near	malloc_failed
+	jz		near malloc_failed
 	mov 	[buffer], eax
 	printf 	malloc_success_msg
 
-; strcpy(buffer, "Hell!\n")
+; manual equivalent of strcpy(buffer, "Hell!\n")
 	mov 	eax, [buffer]
 	mov byte [eax + 0], 'H' 
 	mov byte [eax + 1], 'e' 
@@ -78,26 +79,34 @@ _main:
 	
 ; printf(buffer)
 	printf 	[buffer]
+
+; strcpy(buffer, message)
+	push 	message
+	push 	dword [buffer]
+	call 	_strcpy
+	
+; printf(buffer)
+	printf 	[buffer]
 	
 ; sprintf(buffer, "%d\n",42) 
-	sub 	esp, 12
+	sub 	esp, 3*4
  	push	dword 42
  	push	dword digit_format
  	push	dword buffer
  	call 	_sprintf
-	add 	esp, 12
+	add 	esp, 3*4
 
 ; printf(buffer)
 	printf buffer
 
 ; sprintf(buffer, "%d%d\n",42, 99) 
-	sub 	esp, 12
+	sub 	esp, 4*4
  	push	dword 99
  	push	dword 42
  	push	dword two_digits_format
  	push	dword buffer
  	call 	_sprintf
-	add 	esp, 12
+	add 	esp, 4*4
 
 ; printf(buffer)
 	printf buffer
@@ -119,13 +128,14 @@ exit:
 section .data
 
 ; printf	
-	new_line			db  10, 0
-	string_format		db '%s', 0
-	digit_print			db '%d',10, 0
+	new_line		db  10, 0
+	string_format	db '%s', 0
+	digit_print		db '%d',10, 0
+	message			db 'FooBarQix',10, 0
 ; scanf
 	digit_format		db '%d', 10, 0
 	two_digits_format	db '%d%d', 10, 0
 ; malloc
 	malloc_success_msg	dw 'malloc success', 10, 0
 	malloc_failed_msg	dw 'malloc failed', 10, 0
-	buffer			dw 0  
+	buffer				dw 0  
